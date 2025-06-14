34681a9c9089149b477faf1b7eeb59a924f575ae
manudiv15 <38869988+manudiv16@users.noreply.github.com>
2025-06-15 01:24:20 +0200

    feat: Configure clerkMiddleware to specify authorized parties

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,5 @@
 import { clerkMiddleware } from "@clerk/astro/server";
 
-export const onRequest = clerkMiddleware();
+export const onRequest = clerkMiddleware({
+    authorizedParties: ["https://manudev.dev"],
+});
